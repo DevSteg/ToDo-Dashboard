4a799c5763d0c41ef495ec986d062b198d0b673f
James Stephens <j.l.stephens91@gmail.com>
2020-11-22 22:27:55 +0000

    Add weather.js file, add variables and weather object

---
+++ assets/js/weather.js
@@ -0,0 +1,11 @@
+// Weather Section
+const cityName = document.querySelector('.location');
+const temp = document.querySelector('.temp');
+const weatherIcon = document.querySelector('.weather-icon');
+
+// let apiKey = 0fc41264887289d9795ff5c0a989c31b;
+
+// Empty weather object to be filled using API
+let weather = {};
+
+
