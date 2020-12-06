3299f4b883bfd994d4f8f25e3c06d6b424e1999c
James Stephens <j.l.stephens91@gmail.com>
2020-12-06 17:41:29 +0000

    Refactor the Todo list JS, Add enter key function to todo list

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,59 +3,78 @@ const todoBtn = document.querySelector('.todo-btn');
 const addTodo = document.getElementById('todo-input');
 const todoList = document.querySelector('.todo-list');
 
-// Creates new button element for the check button
-let checkBtn = document.createElement('button');
-checkBtn.classList.add('check-btn', 'icon-btn');
-checkBtn.innerHTML = '<i class="far fa-circle"></i>';
+todoBtn.addEventListener('click', function(){
+    if(addTodo.value === '') {
+        alert('Please Enter a Todo!');
+    } else {
+        addNewTodo();
+    }
+})
+addTodo.addEventListener('keyup', function(){
+    if(event.keyCode === 13) {
+        if(addTodo.value === '') {
+            alert('Please Enter a Todo!');
+        } else {
+            addNewTodo();
+        }
+    }
+})
 
-// Creates new button element for the delete button
-let deleteBtn = document.createElement('button');
-deleteBtn.classList.add('delete-btn', 'icon-btn');
-deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
 
 // Function to add a new todo once the .todo-btn has been clicked
-todoBtn.onclick = function (newTodo) {
+function addNewTodo(newTodo) {
     newTodo = addTodo.value;
-
-    // Creates new LI element
+    
+    // Creates new li element
     let todoItem = document.createElement('li');
     todoItem.classList.add('list-item');
     todoItem.innerHTML = newTodo;
-    
-    if (newTodo === '') {
-        // Checks if #todo-input has no input, then alerts the user if it is empty
-        alert('Please Enter A Todo!');
-    } else {
-        // If #todo-input has a value, enters it into the .todo-list
-        todoItem.prepend(checkBtn); // Requires fixing, Doesn't prepend and append correctly
-        todoItem.append(deleteBtn); // Requires fixing, Doesn't prepend and append correctly
-        todoList.appendChild(todoItem);
-    }
-    // Resets the #todo-input.
-    addTodo.value = '';
-}
 
-// Function to make the check button work
-checkBtn.addEventListener('click', function() {
+    // Creates new div for the check button and delete button
+    let checkDelDiv = document.createElement('div')
+    todoItem.appendChild(checkDelDiv);
+    
+    // Creates new button element for the check button
+    let checkBtn = document.createElement('button');
+    checkBtn.classList.add('check-btn', 'icon-btn');
+    checkBtn.innerHTML = '<i class="far fa-circle"></i>';
+    checkDelDiv.appendChild(checkBtn);
+    
+    // Adds functionality to the checkBtn
+    checkBtn.addEventListener('click', function() {
     // If .check-green is active removes class and linethrough text, replaces the green checkmark with a circle icon
     if (checkBtn.classList.contains('check-green')) {
         this.innerHTML = '<i class="far fa-circle"></i>';
         this.classList.remove('check-green');
-        this.parentElement.classList.remove('complete');
-    // If .check-green is not active, adds the class and linethrough text, replaces the circle with a green checkmark icon
+        checkDelDiv.parentElement.classList.remove('complete');
     } else {
-        this.innerHTML = '<i class="fas fa-check-circle"></i>';
-        this.classList.add('check-green');
-        this.parentElement.classList.add('complete');      
-    }
-})
+        // If .check-green is not active, adds the class and linethrough text, replaces the circle with a green checkmark icon
+            this.innerHTML = '<i class="fas fa-check-circle"></i>';
+            this.classList.add('check-green');
+            checkDelDiv.parentElement.classList.add('complete');
+        }
+    })
+    
+    // Creates new button element for the delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn', 'icon-btn');
+    deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    checkDelDiv.appendChild(deleteBtn);
 
-// Fucntion to make the delete button work
-deleteBtn.addEventListener('click', function() {
-    // removes the parent li element
-    this.parentElement.remove();
-})
+    // Add Functionality to the deleteBtn
+    deleteBtn.addEventListener('click', function() {
+        // removes the parent li element
+        checkDelDiv.parentElement.remove();
+    })
 
+    todoList.appendChild(todoItem);
+
+    // Resets the #todo-input.
+    addTodo.value = '';
+}
+
+
+// Pull todoArr from local stroage
 
 // Time Section
 
@@ -109,4 +128,3 @@ function todaysDate() {
 
 // Call the two functions to add Time and date to index.html when the page loads
 window.onload = clock(), todaysDate();
-
